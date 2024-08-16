506615af3667f79b420ef63975c911c96b15e890
Bianca <biancalenineb.schons@hotmail.com>
2024-08-16 15:37:22 -0300

    loops com for

---
+++ disciplinas.py
@@ -0,0 +1,6 @@
+disciplinas = ("Português", "Matemática", "História", "Geografia", "Filosofia", "Química", "Física", "Biologia")
+
+notas =[8.5, 7.0, 9.5, 9.0, 10, 7.5, 5.5, 6,0]
+
+for idx in range(len(disciplinas)):
+    print(f"A nota de {disciplinas[idx]} foi {notas[idx]}")
---
+++ repetições.py
@@ -0,0 +1,11 @@
+#Iterações Definidas
+alunos = ("Ana", "Tadeu", "Pietra", "Maria Eduarda")
+for nome in alunos:
+    print(nome)
+    print("------")
+
+for numero in range(2, 10):
+    print(numero)
+
+for idx in range(len(alunos)):
+    print(alunos[idx])
