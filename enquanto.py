b717cedde6444381c71b36a8747d38ea92da13a4
Bianca <biancalenineb.schons@hotmail.com>
2024-09-10 18:42:53 -0300

    repetições condicionais

---
+++ enquanto.py
@@ -0,0 +1,8 @@
+nota = float(input("Digite um valor de nota:"))
+# if nota >= 0 and nota <= 10:
+# print(nota)
+# else:
+# print("nota inválida")
+while nota < 0 or nota > 10:
+    nota = float(input("Digite um valor VÁLIDO de nota:"))
+    print(nota)
